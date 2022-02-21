d385b31319428302a58f45ba4842d6912de505ff
Unathi Khoba <ukhoba@student.wethinkcode.co.za>
2022-02-21 17:17:21 +0200

    Updated tables to autoincrement id, and created a new table..

--- User.sql
+++ User.sql
@@ -3,14 +3,13 @@ GO
 
 INSERT INTO Users
     (
-    [user_id],
     [name],
     [email]
     )
-VALUES  (1, 'Tiaan', 'tiaant@bbd.co.za'),
-        (2, 'Vianka', 'viankan@bbd.co.za'),
-        (3, 'UNathi', 'unathi@bbd.co.za'),
-        (4, 'Pieter', 'pieterk@bbd.co.za'),
-        (5, 'Tarshen', 'tarshenn@bbd.co.za'),
-        (6, 'Jacus', 'jacus@bbd.co.za')
+VALUES  ('Tiaan', 'tiaant@bbd.co.za'),
+        ('Vianka', 'viankan@bbd.co.za'),
+        ('UNathi', 'unathi@bbd.co.za'),
+        ('Pieter', 'pieterk@bbd.co.za'),
+        ('Tarshen', 'tarshenn@bbd.co.za'),
+        ('Jacus', 'jacus@bbd.co.za')
 GO
--- route.sql
+++ route.sql
@@ -2,19 +2,18 @@ USE PubCrawl
 GO
 
 CREATE TABLE Route (
-	route_id int NOT NULL PRIMARY KEY,
+	route_id int NOT NULL PRIMARY KEY IDENTITY(1,1),
 	name varchar(50) NOT NULL,
 	user_id int NOT NULL FOREIGN KEY REFERENCES Users(user_id)
 );
 
 INSERT INTO Route
     (
-    [route_id],
     [name],
     [user_id]
     )
-VALUES  (1, 'test1', 1),
-        (2, 'test2', 1),
-        (3, 'test3', 5),
-        (4, 'test4', 3)
+VALUES  ('test1', 1),
+        ('test2', 1),
+        ('test3', 5),
+        ('test4', 3)
 GO
