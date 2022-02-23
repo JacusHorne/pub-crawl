7caa173b83937982a666b12bdf0e8d4ecbe6cec5
Unathi Khoba <ukhoba@student.wethinkcode.co.za>
2022-02-23 23:39:57 +0200

    Updated format to use groups standard

--- Queries/views.sql
+++ Queries/views.sql
@@ -1,12 +1,13 @@
 USE PubCrawl
 GO
 
-CREATE VIEW top_routes
+CREATE VIEW [top_routes]
 AS
-SELECT route.route_id,
-	name,
-	AVG(route_rating.rating)
-FROM Route as route, Route_Rating AS route_rating
-WHERE route_rating.route_id = route_rating.route_id
-GROUP BY route_name;
+SELECT
+	[route].[name],
+	AVG([route_rating].[rating]) AS [rating]
+FROM [dbo].[Route] AS [route], [dbo].[Route_Rating] AS [route_rating]
+WHERE [route].[route_id] = [route_rating].[route_id]
+GROUP BY [route].[name]
+ORDER BY [rating] DESC OFFSET 0 ROWS;
 GO
