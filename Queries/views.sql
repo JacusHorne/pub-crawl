1d4e94141613903baed44c627c8e585b720bfb16
Unathi Khoba <ukhoba@student.wethinkcode.co.za>
2022-02-24 14:29:02 +0200

    Updated top routes view to include a total number of stops for a route

--- Queries/views.sql
+++ Queries/views.sql
@@ -9,9 +9,15 @@ GO
 
 CREATE VIEW [v_Top_Routes]
 AS
-	SELECT r.[name], AVG(rr.[rating]) AS [average_rating]
-	FROM [dbo].[Route] r, [dbo].[Route_Rating] rr
-	WHERE r.[route_id] = rr.[route_id]
+	SELECT 
+		r.[name],
+		AVG(rr.[rating]) AS [average_rating],
+		COUNT(rs.[venue_id]) AS [total_stops]
+	FROM
+		[dbo].[Route] r,
+		[dbo].[Route_Rating] rr,
+		[dbo].[Route_Stop] rs
+	WHERE r.[route_id] = rr.[route_id] AND r.[route_id] = rs.[route_id]
 	GROUP BY r.[name]
 	ORDER BY [average_rating] 
 	DESC OFFSET 0 ROWS;
