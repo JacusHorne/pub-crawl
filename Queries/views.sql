440cc735b1de6bc14c714d7a12cbdca4aa0d07a8
Jacus Horne <jacus@bbd.co.za>
2022-02-24 08:50:29 +0200

    added drinkprice and venuerating views

--- Queries/views.sql
+++ Queries/views.sql
@@ -1,3 +1,4 @@
+
 USE PubCrawl
 GO
 
@@ -10,4 +11,24 @@ FROM [dbo].[Route] AS [route], [dbo].[Route_Rating] AS [route_rating]
 WHERE [route].[route_id] = [route_rating].[route_id]
 GROUP BY [route].[name]
 ORDER BY [rating] DESC OFFSET 0 ROWS;
+GO
+
+-- Average drink price
+CREATE VIEW vDrinkPrice
+AS
+	SELECT d.[name], AVG(vd.[price]) AS average_price
+	FROM [dbo].[Drink] d
+		JOIN [dbo].[Venue_Drink] vd ON d.[drink_id] = vd.[drink_id]
+	GROUP BY d.[name]
+GO
+
+-- Average venue rating (keep rounding?)
+CREATE VIEW vVenueRating
+AS
+	SELECT v.[name], AVG(rr.[rating]) AS average_rating
+	FROM [dbo].[Venue] v
+		JOIN [dbo].[Route_Stop] rs ON v.[venue_id] = rs.[venue_id]
+		JOIN [dbo].[Route] r ON rs.[route_id] = r.[route_id]
+		JOIN [dbo].[Route_Rating] rr ON r.[route_id] = rr.[route_id]
+	GROUP BY v.[name]
 GO
